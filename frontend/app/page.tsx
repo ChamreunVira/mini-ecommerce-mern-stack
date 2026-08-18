2025a2dee85ff59a522080277a5aa16549328093
ChamreunVira <virachamreun@gmail.com>
2026-08-18 18:43:32 +0700

    _

---
+++ frontend/app/page.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='h-screen w-full flex items-center justify-center'>
+
+      <div>
+        <h1 className='font-3xl font-semibold tracking-wider'>Chalange Building Mini-Ecommerce</h1>
+        <ul>
+          <li>Category</li>
+          <li>Product</li>
+          <li>Cart</li>
+          <li>Order</li>
+          <li>Checkout</li>
+          <li>Shipment</li>
+          <li>Promo code</li>
+          <li>KHQR Payment + Telegram Notification</li>
+        </ul>
+      </div>
+
+    </div>
+  )
+}
+
+export default Home

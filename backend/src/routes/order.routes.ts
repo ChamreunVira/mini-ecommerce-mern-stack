70e88bf37c8224de867d315f2143e2dac8527a5e
ChamreunVira <virachamreun@gmail.com>
2026-08-26 17:42:41 +0700

    feat: order route

--- backend/src/routes/order.routes.ts
+++ backend/src/routes/order.routes.ts
@@ -4,6 +4,8 @@ import { authenticat } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
+router.get("/", authenticat, orderController.getUserOrders);
 router.post("/", authenticat, orderController.create);
+router.get("/:id", authenticat, orderController.getOrderById);
 
 export default router;
